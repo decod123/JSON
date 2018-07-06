db6fbd98319ba2325473857d7444f6933f4d6d67
decod123 <decod_decod@yahoo.com>
2018-07-06 08:47:36 +0200

    Add files via upload
    Add an optional extended description…

---
+++ ARM-VirtualMachine/Deploy-ContosoIaas_3.ps1
@@ -0,0 +1,21 @@
+﻿### Define Deployment Variables
+{
+$location = '[resource group location]'
+$resourceGroupName = '[resource group name]'
+$resourceDeploymentName = 'contoso-iaas-deployment'
+$templatePath = $env:SystemDrive + '\Users\LabUser\Documents\HOL_Lab_Files\3_Deploying_Virtual_Machines'
+$templateFile = 'contosoIaaS_3.json'
+$template = $templatePath + '\' + $templateFile
+$password = "C0nts0sP@55"
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+}
+
+### Deploy Resources
+{
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
